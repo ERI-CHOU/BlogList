24e61f777efb068d0aba642768294c1c4f294f02
ERI-CHOU <dsayzhanglin@gmail.com>
2022-06-13 13:20:55 +0900

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,9 +3,20 @@ const totalLikes = (blogs) => {
     return sum + val.likes
   }
 
-  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+  return blogs.length === 
+    0 ? 0 
+    : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes), -1)
+  
+  return blogs.length === 0 
+    ? {} 
+    : (({author, likes, title}) => ({ author, likes, title }))(blogs.find(blog => blog.likes === maxLikes))
 }
 
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
